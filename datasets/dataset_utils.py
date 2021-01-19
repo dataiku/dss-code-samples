027a567d9526953361480bede48d1b55fc409bc4
Jean-Yves Gerardy <jygerardy@DKUMBPjygerardy.lan>
2021-01-19 15:15:48 -0500

    Add dynamic partition build

--- datasets/dataset_utils.py
+++ datasets/dataset_utils.py
@@ -14,3 +14,19 @@ def get_last_build_date(client=None, project_key=None, dataset=None):
 	dt = datetime.fromtimestamp(last_modif/1000)
 	return dt
 
+def build_todays_partition(client=None, project_key=None, dataset=None):
+	 """Build parition of today's date in specified dataset.
+	 Return status of build. 
+    Args:
+        client: A handle on the target DSS instance.
+        project_key: A string representing the target project key.
+        dataset: name of dataset,
+    """
+	now = datetime.now()
+	partition = now.strftime("%Y-%m-%d")
+	project = client.get_project(project_key)
+	dataset = project.get_dataset(dataset)
+	job = dataset.build(partitions=partition)
+	return job.get_status()
+
+
