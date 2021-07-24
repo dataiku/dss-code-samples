d88f5f7b94fada958a9191e1c9792cc1850c6e6b
Elias Habash <elias.habash@dataiku.com>
2021-07-23 18:43:41 -0700

    Deleting a deployment on the API Deployer

---
+++ api_deployer/delete_a_deployment/README.md
@@ -0,0 +1,9 @@
+# Deleting a deployment on the API Deployer programmatically inside DSS
+
+## Questions
+
+How can deployments already pushed to the API Deployer be deleted programmatically inside DSS?
+
+## Answers
+
+You can delete a deployment by leveraging the API Deployer Python APIs to first disable the deployment and then delete it.
---
+++ api_deployer/delete_a_deployment/code.py
@@ -0,0 +1,19 @@
+import dataiku
+
+def delete_deployment(deploymentId=None):
+    """Delete a deployment on the API Deployer inside DSS
+    """ 
+    # Instantiate client
+    client = dataiku.api_client()
+    
+    # Retrieve deployment and disable it first
+    deployer = client.get_apideployer()
+    deployment = deployer.get_deployment(deploymentId)
+    settings = deployment.get_settings()
+    settings.set_enabled(enabled=True)
+    settings.save()
+
+    # Delete deployment once it has been disabled
+    update = deployment.start_update()
+    update.wait_for_result()
+    deployment.delete()
